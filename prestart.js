07e372a4478e1903040a43f6521249d2eecd3c67
conorlawton <conor.lawton1@gmail.com>
2022-06-21 00:49:24 +0100

    Fixed issue with head alignment

--- prestart.js
+++ prestart.js
@@ -3,6 +3,22 @@ import "./js/bgm/custom-bgm.js";
 sc.PARTY_OPTIONS.push("triblader2");
 sc.PARTY_OPTIONS.push("triblader3");
 
+ig.EVENT_STEP.START_PVP_BATTLE.inject({
+  init(a) {
+    this.parent(a);
+
+    let b = 110 + sc.pvp.winPoints * 10;
+    b += (sc.party.getPartySize() + 1) * 16;
+    b += a.enemies.length * 16;
+    
+    const combatHudGui = ig.gui.guiHooks.find(ui => 
+      ui.gui instanceof sc.CombatHudGui
+    );
+
+    combatHudGui.gui.upperGui.sub.pvp.setSize(b, 20);
+  }
+});
+
 sc.Arena.inject({
     init(){
         this.parent()
